544b606473909d941a57820478601c88f2d00c2f
Brice Copy <brice.copy@gmail.com>
2015-04-02 16:02:45 +0200

    Updated site

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/theme/sass"
+generated_images_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/.cache/compass/images"
+images_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/theme/images"
+fonts_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/theme/fonts"
+
+line_comments = false
+output_style = :compressed
