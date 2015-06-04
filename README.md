4ccfdc2c0561788ce1a34d268745e47bd0e85abb
bcopy <brice.copy@gmail.com>
2015-06-04 15:53:01 +0200

    Updated site

--- README.md
+++
@@ -1,4 +0,0 @@
-# raspbuggy
-A Raspberry PI-based autonomous car
-
-Github pages website branch - published at http://cmcrobotics.github.io/raspbuggy
--- config.rb
+++ config.rb
@@ -8,13 +8,13 @@ http_path = "/"
 http_images_path = "/images"
 http_generated_images_path = "/images"
 http_fonts_path = "/fonts"
-css_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/.cache/compass/stylesheets"
+css_path = "/var/lib/jenkins/jobs/Raspbuggy Website/workspace/website/octopress-grain/.cache/compass/stylesheets"
 
 # Local development paths
-sass_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/theme/sass"
-generated_images_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/.cache/compass/images"
-images_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/theme/images"
-fonts_path = "/home/bcopy/workspace/raspbuggy/website/octopress-grain/theme/fonts"
+sass_path = "/var/lib/jenkins/jobs/Raspbuggy Website/workspace/website/octopress-grain/theme/sass"
+generated_images_path = "/var/lib/jenkins/jobs/Raspbuggy Website/workspace/website/octopress-grain/.cache/compass/images"
+images_path = "/var/lib/jenkins/jobs/Raspbuggy Website/workspace/website/octopress-grain/theme/images"
+fonts_path = "/var/lib/jenkins/jobs/Raspbuggy Website/workspace/website/octopress-grain/theme/fonts"
 
 line_comments = false
 output_style = :compressed
