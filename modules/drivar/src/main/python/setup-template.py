35405d8e1c26c8c3552e13c7d056f82e9b1c410a
bcopy <brice.copy@gmail.com>
2016-07-24 11:18:54 +0200

    Added drivar python package complete with dependencies

--- modules/drivar/src/main/python/setup-template.py
+++ modules/drivar/src/main/python/setup-template.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-      install_requires=['distribute'],
+      install_requires=['distribute', 'Adafruit_MotorHAT>=1.3.0','nxt-python==2.2.2'],
       name = '${PROJECT_NAME}',
       description = 'Hardware abstraction layer for Raspbuggy',
       author = 'Brice Copy',
       url = 'https://github.com/cmcrobotics/raspbuggy',
       keywords = ['raspbuggy'],
       version = '${VERSION}',
-      packages = ['drivar']
+      packages =  find_packages('.')
 )
